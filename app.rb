70fdcad5a9e26e137d517031400bd131ff7a965e
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-08-06 18:23:07 +0000

    match target

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'net/http'
+require 'json'
 
+# Home route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  uri = URI("https://api.exchangerate.host/symbols")
+  response = Net::HTTP.get(uri)
+  @symbols = JSON.parse(response)["symbols"]
+
+  erb :home
+end
+
+# Single currency route
+get("/:currency") do |currency|
+  uri = URI("https://api.exchangerate.host/symbols")
+  response = Net::HTTP.get(uri)
+  @currency = currency
+  @symbols = JSON.parse(response)["symbols"]
+
+  erb :currency
+end
+
+# Pair of currencies route
+get("/:currency1/:currency2") do |currency1, currency2|
+  uri = URI("https://api.exchangerate.host/convert?from=#{currency1}&to=#{currency2}")
+  response = Net::HTTP.get(uri)
+  @currency1 = currency1
+  @currency2 = currency2
+  @conversion_rate = JSON.parse(response)["info"]["rate"]
+
+  erb :conversion
 end
